11aafc6abf73e1945082884119117b5a3f9378f3
Daniel-MSoares <munizsoaresrapper@gmail.com>
2022-10-23 12:28:47 -0300

    Feed,loading,tema e fontes,compnetiza header

---
+++ src/components/Loading.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { VStack,Skeleton,Center, HStack } from 'native-base';
+
+export function Loading() {
+    
+  return (
+    <Center w="100%" pt={10} >
+      <VStack w="90%" maxW="400" h="full"   space={8} overflow="hidden" rounded="md" >
+        <Skeleton h="10" w={190} alignSelf='center' fadeDuration={0.2}/>
+        <Skeleton h="10" w='full' fadeDuration={0.2} fa/>
+        
+        <HStack>
+        <Skeleton h="10" w='2/3' pr={2} fadeDuration={0.2}/>
+        <Skeleton h="10" w='1/3'  fadeDuration={0.2}/>
+        </HStack>
+        <Skeleton.Text mt={-4} lines={1} w='1/2'  fontSize={"md"} fadeDuration={1.5}/>
+
+        <HStack>
+          <Skeleton rounded='md' height={230} w={180} mr={4} fadeDuration={0.4}></Skeleton>
+          <Skeleton rounded='md' height={230} w={180} fadeDuration={0.4}></Skeleton>
+        </HStack>
+        <HStack>
+          <Skeleton rounded='md' height={230} w={180} mr={4} fadeDuration={0.4}></Skeleton>
+          <Skeleton rounded='md' height={230} w={180} fadeDuration={0.4}></Skeleton>
+        </HStack>
+        <HStack>
+          <Skeleton rounded='md' height={230} w={180} mr={4} fadeDuration={0.4}></Skeleton>
+          <Skeleton rounded='md' height={230} w={180} fadeDuration={0.4}></Skeleton>
+        </HStack>
+      </VStack>
+    </Center>
+  );
+}
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,116 @@
-import {Button} from '../components/Button'
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { VStack,HStack,Heading,Box,useTheme,FlatList, Center, Text } from 'native-base';
+import {TouchableOpacity} from 'react-native';
+import {MaterialIcons,Feather}from '@expo/vector-icons'
+import { Header } from '../components/Header';
+import { ProductCard } from '../components/ProductCard';
+
 
 export function Home() {
+  const {colors}=useTheme();
+  const [order,setOrder]=useState(true)
+  
+  let products=[
+      { 
+        id:1,
+        img:require("../assets/exemplo.jpeg"),
+        title:"Tomate",
+        description:"pequena descrição sobre oproduto",
+        price:4.50,
+        favorite:true
+      },
+      { 
+        id:2,
+        img:require("../assets/exemplo.jpeg"),
+        title:"Alface",
+        description:"pequena descrição sobre oproduto",
+        price:2.50, 
+        favorite:false
+      },
+
+      { 
+        id:3,
+        img:require("../assets/exemplo.jpeg"),
+        title:"Repolho",
+        description:"pequena descrição sobre oproduto",
+        price:3.50, 
+        favorite:false
+      },
+
+      { 
+        id:4,
+        img:require("../assets/exemplo.jpeg"),
+        title:"Abóbora",
+        description:"pequena descrição sobre oproduto",
+        price:3.50, 
+        favorite:true
+      },
+      { 
+        id:5,
+        img:require("../assets/exemplo.jpeg"),
+        title:"cenoura",
+        description:"pequena descrição sobre oproduto",
+        price:3.50, 
+        favorite:true
+      },
+      { 
+        id:6,
+        img:require("../assets/exemplo.jpeg"),
+        title:"Kit frutas",
+        description:"pequena descrição sobre oproduto",
+        price:3.50, 
+        favorite:false
+      },
+      { 
+        id:7,
+        img:require("../assets/exemplo.jpeg"),
+        title:"Abóbora",
+        description:"pequena descrição sobre oproduto",
+        price:3.50, 
+        favorite:false
+      },
+
+    ]
+
+
   return (
-    <View>
-      <Button />
-    </View>
+    <VStack flex={1} width='full' bg='gray.200'
+            p={8} pt={0} alignItems='center'  
+            px={4} pb={0}>
+      <Header/>
+     
+      <HStack mt={2} alignItems="center" alignContent='center' justifyContent='space-between' w='full'>
+        <Heading size='2xl' color='primary.600'>Produtos</Heading>
+        <Box flexDirection='row' >
+          <TouchableOpacity style={{ marginRight: 10 }} onPress={()=>setOrder(!order)}>
+            <MaterialIcons name="unfold-more" size={40}  color={colors.primary[600]} />
+          </TouchableOpacity>
+          <TouchableOpacity disabled>
+            <Feather name="filter" size={36} color={colors.primary[600]}/>
+          </TouchableOpacity>
+        </Box>
+      </HStack>   
+      <Heading size='sm' color='gray.600' justifyItems='left' w='full' mb={4}>Todos os produtos</Heading>
+      <FlatList
+        data={order ? products : products.reverse()}
+        showsVerticalScrollIndicator={false}
+        w='full'
+        contentContainerStyle={{ paddingBottom: 100}}
+        numColumns='2'
+        
+        keyExtractor={item=>item.id}
+        renderItem={({item})=><ProductCard data={item}/>}
+        ListEmptyComponent={() => (
+          
+          <Center alignContent='center' flex={1} h={400}>
+            <Text color="gray.300" fontSize="4xl" textAlign="center">
+              :({'\n'}
+              Oops! Não há Produtos para mostrar.
+            </Text>
+          </Center>
+        )}
+      />
+      
+    </VStack>
   );
 }
