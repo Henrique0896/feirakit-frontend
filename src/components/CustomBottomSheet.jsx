25e2076ab93be36daa7d221c200369054e2f40c0
Daniel-MSoares <munizsoaresrapper@gmail.com>
2022-12-10 12:57:11 -0300

    ajusta customBottomSheet

--- src/components/CustomBottomSheet.jsx
+++ src/components/CustomBottomSheet.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { Heading, HStack, VStack,useTheme } from 'native-base';
-import{MaterialIcons} from '@expo/vector-icons'
-import { TouchableOpacity } from 'react-native';
-export function CustomBottomSheet({actionGallery,actionCamera}) {
-
+import React from "react";
+import { Heading, HStack, VStack, useTheme } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+export function CustomBottomSheet({ actionGallery, actionCamera }) {
   const { colors } = useTheme();
   return (
-    <HStack justifyContent='center' >
-        <VStack w='45%' h='90%' alignItems='center' justifyContent='center'>
-          <TouchableOpacity onPress={actionCamera}>
-           <MaterialIcons name='camera-alt' size={70} color={colors.blue[700]}/>
-           <Heading color={colors.blue[700]}>Câmera</Heading>
-           </TouchableOpacity>
+    <HStack justifyContent="center" mt={4}>
+      <TouchableOpacity
+        style={{ width: "45%", height: "100%" }}
+        onPress={actionCamera}
+      >
+        <VStack alignItems="center" justifyContent="center">
+          <MaterialIcons name="camera-alt" size={70} color={colors.blue[700]} />
+          <Heading color={colors.blue[700]}>Câmera</Heading>
         </VStack>
-        <VStack w='45%' h='90%' alignItems='center' justifyContent='center'>
-          <TouchableOpacity onPress={actionGallery}>
-           <MaterialIcons name='photo' size={70} color={colors.blue[700]} />
-           <Heading color={colors.blue[700]}>Galeria</Heading>
-           </TouchableOpacity>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{ width: "45%", height: "100%" }}
+        onPress={actionGallery}
+      >
+        <VStack alignItems="center" justifyContent="center">
+          <MaterialIcons name="photo-library" size={70} color={colors.blue[700]} />
+          <Heading color={colors.blue[700]}>Galeria</Heading>
         </VStack>
+      </TouchableOpacity>
     </HStack>
   );
 }
