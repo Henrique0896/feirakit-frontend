e7408e9154b12f0eae3580877d05eb4a8825532e
Geraldo Otávio Figueiredo <geraotavio@hotmail.com>
2023-01-18 22:38:29 -0300

    update

--- src/services/ApiFeiraKit.js
+++ src/services/ApiFeiraKit.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {API_URL}from '@env';
 const apiFeiraKit= axios.create({
-   baseURL:API_URL,
+   baseURL:'http://192.168.3.2:5000/',
    headers: {
     'accept':'application/json',
     'Content-Type': 'application/json'
