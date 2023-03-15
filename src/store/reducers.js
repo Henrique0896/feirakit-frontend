53ff59791e68ee29a7bab941f2ece21978e333c6
Geraldo Otávio Figueiredo <geraotavio@hotmail.com>
2023-03-15 10:21:30 -0300

    login

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,13 +1,6 @@
 const initialState = {
-  authToken: "63c895c81060e0d6ba422c19",
-  userData: {
-    "id": "63c895c81060e0d6ba422c19",
-    "nome": "Geraldo",
-    "email": "geraotavio@hotmail.com",
-    "telefone": "33998785878",
-    "endereco": "Santos Dumont, 30, Centro, Leme do Prado - MG,39655-000",
-    "senha": "geraldo1230"
-  },
+  authToken: null,
+  userData: {},
   anyData: [],
 };
 
@@ -16,7 +9,7 @@ export default (state = initialState, action) => {
     case "LOGIN":
       return {
         ...state,
-        authToken: action.payload.id,
+        authToken: action.payload.email,
         userData:action.payload
       };
     case "LOGOUT":
